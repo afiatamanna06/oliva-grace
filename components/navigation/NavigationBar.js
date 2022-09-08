4b83ce62ca78894e210cc08e9bd7bb93c7165c25
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-09 00:34:46 +0600

    Add: Update navigation bar

--- components/navigation/NavigationBar.js
+++ components/navigation/NavigationBar.js
@@ -1,12 +1,27 @@
-import {  Button, Flex, Box } from '@chakra-ui/react'
+import {  Button, Flex, Box, Link as ChakraLink, Heading } from '@chakra-ui/react'
 import Link from 'next/link'
 import { FaFacebookF } from "react-icons/fa"
+import navigationLinks from './NavigationLinks'
 
 function NavigationBar() {
   return (
-    <Box>
-
-    </Box>
+    <Flex position="fixed" alignItems="center" justify="space-between" bgColor="#F2E9E4" w="full" p="3">
+        <Flex gap={100} alignItems="center" justify="center">
+            {navigationLinks.slice(0, 2).map(({name, path}) => (
+                <Link key={name} href={path} passHref>
+                    <ChakraLink>{name}</ChakraLink>
+                </Link>
+            ))}
+            <Link href="/" passHref>
+                <Heading>OLIVA GRACE</Heading>
+            </Link>
+            {navigationLinks.slice(2, 4).map(({name, path}) => (
+                <Link key={name} href={path} passHref>
+                    <ChakraLink>{name}</ChakraLink>
+                </Link>
+            ))}
+        </Flex>
+    </Flex>
   )
 }
 
---
+++ components/navigation/NavigationLinks.js
@@ -0,0 +1,20 @@
+const navigationLinks = [
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+        name: "About",
+        path: "#about",
+    },
+    {
+        name: "Services",
+        path: "#services",
+    },
+    {
+        name: "Contact",
+        path: "#contact",
+    },
+]
+
+export default navigationLinks
