42d06844afe5196d4a33bf5791bb851da236ac10
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-09 00:05:18 +0600

    first commit

---
+++ components/navigation/NavigationBar.js
@@ -0,0 +1,13 @@
+import {  Button, Flex, Box } from '@chakra-ui/react'
+import Link from 'next/link'
+import { FaFacebookF } from "react-icons/fa"
+
+function NavigationBar() {
+  return (
+    <Box>
+
+    </Box>
+  )
+}
+
+export default NavigationBar
