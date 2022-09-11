34b22f58156c0b1fba210357fb10bb759659f048
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-11 20:15:56 +0600

    Add: Add services list data

---
+++ components/services/ServicesList.js
@@ -0,0 +1,22 @@
+const servicesList = [
+    {
+        image: "/services_1.png",
+        title: "SERVICE PACKAGE 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi neque eget dictum enim.", 
+        price: "STARTING PRICE OF $100"
+    },
+    {
+        image: "/services_2.png",
+        title: "SERVICE PACKAGE 2",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi neque eget dictum enim.", 
+        price: "STARTING PRICE OF $150"
+    },
+    {
+        image: "/services_3.png",
+        title: "SERVICE PACKAGE 3",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi neque eget dictum enim.", 
+        price: "STARTING PRICE OF $200"
+    },
+]
+
+export default servicesList
