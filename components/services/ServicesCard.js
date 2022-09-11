216091236e18f89aebaf8ff6c3ee71e30b1631be
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-11 19:40:53 +0600

    Add: Add basic services card file

---
+++ components/services/ServicesCard.js
@@ -0,0 +1,11 @@
+import { Flex } from '@chakra-ui/react'
+
+function ServicesCard({ image, title, description, price }) {
+  return (
+    <Flex>
+
+    </Flex>
+  )
+}
+
+export default ServicesCard
