c60b69a7cd0973dfd79c66ec572d0637dae23a0b
afiatamanna06 <afiatamanna06@gmail.com>
2022-09-12 20:21:08 +0600

    Add: Update testimonial and contact section fontsize, weight, and font

--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -7,10 +7,10 @@ const contactImages = ["/contact_1.png", "/contact_2.png", "/contact_3.png", "/c
 
 function Contact() {
   return (
-    <Box h={["65rem", "55rem", "35rem", "20rem", "20rem"]} id="contact" bgColor="#F2E9E4" w="full" pt="100">
+    <Box h={["65rem", "55rem", "35rem", "20rem", "20rem"]} fontFamily="'Poppins', sans-serif" letterSpacing={[1, 1, 1, 1, 3]} id="contact" bgColor="#F2E9E4" w="full" pt="100">
         <Center>
             <Flex maxH={["full", "full", "full", "30rem", "30rem"]} maxW={["full", "full", "full", "95rem", "95rem"]} alignItems="center" direction="column">
-                <Heading mb="8" textAlign="center">FOLLOW ME @OLIVAGRACE</Heading>
+                <Heading mb="8" textAlign="center" fontFamily="'Poppins', sans-serif" letterSpacing={3} fontSize="2xl" fontWeight="semibold">FOLLOW ME @OLIVAGRACE</Heading>
                 <Flex wrap="wrap" gap={14} justify="center" w="full">
                     {contactImages.map((item) => (
                       <Image key={item} w="15rem" h="12rem" objectFit="cover" src={item} alt="" />
@@ -20,7 +20,7 @@ function Contact() {
         </Center>
         <Center>
           <Flex direction={["column", "column", "column", "row", "row"]} mt="16" mb="10" gap={[5, 5, 5, 0, 0]} justify="space-between" alignItems="center" w={["full", "full", "full", "89rem", "89rem"]}>
-              <Flex gap={10}>
+              <Flex gap={10} fontWeight="medium">
                     {navigationLinks.map(({name, path}) => (
                         <Link key={name} href={path} passHref>
                             <ChakraLink>{name}</ChakraLink>
@@ -28,7 +28,7 @@ function Contact() {
                     ))}
               </Flex>
               <SocialSites color="rgba(0, 0, 0, 1)" />
-              <Box>COPYRIGHT @2022 | OLIVIAGRACE</Box>
+              <Box letterSpacing={3}>COPYRIGHT @2022 | OLIVIAGRACE</Box>
           </Flex>
         </Center>
     </Box>
